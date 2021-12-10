ba018e3fc7dde1397759466aee40420298d23e76
damon <lolmemedon@gmail.com>
2021-12-10 16:21:55 -0500

    Pushing every thing so I can rebase project

--- Sigma3/Sigma3/Objects/User.cs
+++ Sigma3/Sigma3/Objects/User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using SQLite;
+using static Sigma3.Views.AddTooPortfolioPage;
 
 namespace Sigma3.Objects
 {
@@ -14,9 +15,52 @@ namespace Sigma3.Objects
         public string Password { get; set; } 
         public string PhoneNumber { get; set; }
         public decimal PortfolioBalance { get; set; }
-        public List<StockModel> UserFollowing { get; set; } 
-        public List<StockModel> UserPortfolio { get; set; }
+        public List<StockModel> UserFollowing { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public Dictionary<string, UserSecurity> UserPortfolio { get; set; } = new Dictionary<string, UserSecurity>();
 
+        
+        /*
+        public bool AddSecurity(ATPReturnVal stockModel)
+        {
+            Transactions.Add(new Transaction
+            { 
+                TransType = Transaction.TransactionType.BUY,
+                TransactionId = new Guid().ToString(),
+
+            });
+
+            var symbol = stockModel.model.Symbol;
+            if (UserPortfolio.ContainsKey(symbol))
+            {
+                UserPortfolio[symbol].AmountOwned += stockModel.AmountTransfered;
+                UserPortfolio[symbol].AveragePrice = ();
+            }
+        }
+        */
+
+        
+        public class UserSecurity
+        {
+            public StockModel Security { get; set; }
+            public decimal AmountOwned { get; set; } = 0;
+            public decimal AveragePrice { get; set; }
+            public int BuyTimes { get; set; }
+            public int SellTimes { get; set; }
+        }
+
+        public class Transaction
+        {
+            public string TransactionId { get; set; }
+            public TransactionType TransType { get; set; }
+            public decimal AmountTransacted { get; set; }
+
+            public enum TransactionType
+            {
+                BUY,
+                SELL
+            }
+        }
 
 
         

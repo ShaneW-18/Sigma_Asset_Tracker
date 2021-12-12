8ec5158becbbfd16088ee05842c00928e286db15
damon <damon@montague.im>
2021-12-12 05:01:26 -0500

    updated api

--- SigmaTransactionAPI/TransactionModel.cs
+++ SigmaTransactionAPI/TransactionModel.cs
@@ -1,19 +1,29 @@
-﻿namespace SigmaTransactionAPI
+﻿using Newtonsoft.Json;
+
+namespace SigmaTransactionAPI
 {
     public class TransactionModel
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
-        public TransactionType TransType { get; set; }
-        public decimal AmountTransacted { get; set; }
-        public decimal PricePerSecurity { get; set; }  
-        public string SecurityTraded { get; set; }
-        public string UserId { get; set; }
-        public DateTimeOffset DateTime { get; set; }
 
-        public enum TransactionType
-        {
-            BUY,
-            SELL
-        }
+        [JsonProperty("transType")]
+        public string TransType { get; set; }
+
+        [JsonProperty("amountTransacted")]
+        public decimal AmountTransacted { get; set; }
+
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("securityTraded")]
+        public string SecurityTraded { get; set; }
+
+        [JsonProperty("dateTime")]
+        public DateTimeOffset Time { get; set; }
+
+        [JsonProperty("pricePerSecurity")]
+        public decimal PricePerSecurity { get; set; }
+     
     }
 }
